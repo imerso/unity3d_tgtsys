a22034a754beb7458e5110d7add3f1afcfe8d51f
Vander R. N. Dias <imerso@imersiva.com>
2020-05-04 19:06:40 -0300

    Better comments

--- Assets/Scripts/CameraControl_Simple.cs
+++ Assets/Scripts/CameraControl_Simple.cs
@@ -28,29 +28,36 @@ public class CameraControl_Simple : MonoBehaviour
 
 	void Update ()
 	{
+		// escape quits app
 		if (Input.GetKeyDown(KeyCode.Escape))
 		{
 			Application.Quit();
 		}
 
-
+		// right mouse button rotates camera
 		if (Input.GetMouseButtonDown(1))
 		{
+			// started dragging
 			click = true;
 			oldMouse = Input.mousePosition;
 		}
 		else if (Input.GetMouseButtonUp(1))
 		{
+			// stopped dragging
 			click = false;
 		}
 
+		// if right mouse button is clicked,
+		// rotates camera with mouse movements
 		if (click)
 		{
 			Vector3 mouse = Input.mousePosition;
 
+			// scaled delta movement
 			float mx = (mouse.x - oldMouse.x) * 0.1f;
 			float my = (mouse.y - oldMouse.y) * 0.1f;
 
+			// we use a fps-like camera, with euler rotations
 			fpsRot.x -= my;
 			fpsRot.y += mx;
 			fpsRot.x = Mathf.Min(85, Mathf.Max(-85, fpsRot.x));
@@ -59,39 +66,49 @@ public class CameraControl_Simple : MonoBehaviour
 			oldMouse = mouse;
 		}
 
+		// default movement speed
 		float speed = 5;
+
+		// left-shift key for turbo speed
 		if (Input.GetKey (KeyCode.LeftShift))
 		{
 			speed *= 5;
 		}
 
+		// scales speed with time elapsed
 		speed *= Time.deltaTime;
 
+		// forward move
 		if (Input.GetKey (KeyCode.W))
 		{
 			transform.position += transform.forward*speed;
 		}
 
+		// backward move
 		if (Input.GetKey (KeyCode.S))
 		{
 			transform.position -= transform.forward*speed;
 		}
 
+		// left strafe
 		if (Input.GetKey (KeyCode.A))
 		{
 			transform.position -= transform.right*speed;
 		}
 
+		// right strafe
 		if (Input.GetKey (KeyCode.D))
 		{
 			transform.position += transform.right*speed;
 		}
 
+		// moves down
 		if (Input.GetKey(KeyCode.Q))
 		{
 			transform.position -= Vector3.up*speed;
 		}
 
+		// moves up
 		if (Input.GetKey(KeyCode.E))
 		{
 			transform.position += Vector3.up*speed;
